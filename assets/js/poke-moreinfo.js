85fce049f90e422b6c77907dd008f81c2f5c1698
yes i am <132221618+code-yesiam@users.noreply.github.com>
2023-08-31 11:42:53 -0300

    Update poke-moreinfo.js

--- assets/js/poke-moreinfo.js
+++ assets/js/poke-moreinfo.js
@@ -2,3 +2,18 @@ function openInfo() {
     const openPokemonInfo = document.getElementById('info')
     
 }
+
+/* <div id="poke-modal" class="modal">
+                    <div class="poke-modal-content ${pokemon.type}">
+                        <span class="number">#${pokemon.number}</span>
+                        <span class="name">
+                            <a href="#">${pokemon.name}</a>
+                        </span>
+                        <div class="detail">
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                            </ol>
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </div>
+            </div> */
