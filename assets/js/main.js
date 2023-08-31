c2913b9c6876dddb19b2ee4cb528e91f5bc98534
yes i am <132221618+code-yesiam@users.noreply.github.com>
2023-08-31 11:43:11 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonOl = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const pokeModal = document.getElementById('showDetail')
 const limit = 5
 let offset = 0
 
@@ -8,10 +9,10 @@ function loadPokemonItens(offset, limit) {
 
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
             const newHtml = pokemons.map((pokemon) => `
-            <li class="pokemon ${pokemon.type} info" onclick="openInfo()">
+            <li id="${pokemon.name}" class="pokemon ${pokemon.type} info" onclick="openInfo()">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">
-                    <a href="#poke-modal">${pokemon.name}</a>
+                    <a href="#poke-modal" id="showDetail">${pokemon.name}</a>
                 </span>
                     <div class="detail">
                         <ol class="types">
@@ -20,7 +21,8 @@ function loadPokemonItens(offset, limit) {
                         <img src="${pokemon.photo}" alt="${pokemon.name}">
                     </div>
             </li>
-            <div id="poke-modal" class="modal">
+            `).join('')
+            /* <div id="poke-modal" class="modal">
                     <div class="poke-modal-content ${pokemon.type}">
                         <span class="number">#${pokemon.number}</span>
                         <span class="name">
@@ -33,8 +35,7 @@ function loadPokemonItens(offset, limit) {
                         <img src="${pokemon.photo}" alt="${pokemon.name}">
                     </div>
                 </div>
-            </div>
-            `).join('')
+            </div>*/
 
             pokemonOl.innerHTML += newHtml
         }
@@ -46,4 +47,18 @@ loadPokemonItens(offset, limit)
 loadMoreButton.addEventListener('click', () => {
     offset += limit
     loadPokemonItens(offset, limit)
+})
+
+function openInfo(pokemon) {
+    const modalNumber = document.getElementById(`${}`);
+
+    modalNumber.textContent = `#${pokemon.number}`;
+    modalName.textContent = pokemon.name;
+    modalImage.src = pokemon.photo;
+
+    modalTypes.innerHTML = pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('');
+}
+
+pokeModal.addEventListener('click', () => {
+    
 })
